a343dda4176690b015c3e971441aaba9d745daf2
Shivkoirala1 <koirala.khushi456@gmail.com>
2026-08-18 13:59:12 +0545

    Adding the timeout to the create

--- realestate-mern/frontend/src/api/axios.js
+++ realestate-mern/frontend/src/api/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://real-estate-project-p237.onrender.com/api',
+  timeout : 60000,
 });
 
 api.interceptors.request.use((config) => {
